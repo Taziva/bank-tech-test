6c7be864dd81aaf078e64ab8ee5e0f23580ec213
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-03-06 12:18:22 +0000

    add withdraw method and a minimum balance of 0

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,19 @@
 class Account
   attr_reader :balance
+  MIN_BALANCE = 0
   def initialize
-    @balance = 0
+    @balance = MIN_BALANCE
   end
 
   def deposit(money)
     @balance+=money
   end
 
-  
+  def withdraw(money)
+    raise "Insufficient balance in account" if @balance - money < MIN_BALANCE
+    @balance-=money
+  end
+
+
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -16,4 +16,20 @@ describe Account do
       end
     end
   end
+  describe "#withdraw" do
+    context "when withdrawing money from an account" do
+      it "responds to the withdraw method" do
+        expect(account).to respond_to(:withdraw)
+      end
+      it "takes a a specified amount from the account balance" do
+        account.deposit(100)
+        account.withdraw(50)
+        expect(account.balance).to eq(50)
+      end
+      it "can't take out more money than is in the balance" do
+        account.deposit(50)
+        expect{account.withdraw(100)}.to raise_error("Insufficient balance in account")
+      end
+    end
+  end
 end
